515b0b9a9d3400564eabb52d6d263da0223d1d71
goro-o56 <yamashitagoro2727@gmail.com>
2021-12-28 16:57:41 +0900

    apg4b stlcontainer

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -2,29 +2,40 @@
 using namespace std;
 
 int main() {
-  //Setは重複のないデータのまとまりを扱うため
-  //Keyだけのmapという感じ
+  //出現回数が最も多い値とその出現回数
+  //配列をしまう
+  map<int, int> m;
+  int N = 0;
+  set<int> S ;
+
+  cin >> N ;
+  for(int i = 0; i < N; i++) {
+    //mにkey(=i),valueをしまう
+    cin >> m[i];
+    S.insert(m[i]);
+  }
+
+  //辞書の中で最も多く出現する物を総なめする
+  map<int, int> ans; //カウント用のmap
+  int cnt ;
+
+  for(int i = 0; i < S.size() ; i++) {//種類をそうなめ
+    cnt = 0;
+    for(int j = 0; j < N; j++) {
+      //ある最小の種類S[i]を固定した中で、m[j]が含まれるか
+        if(*begin(S) == m[j]){ //最小値から取っていけば順に進められる
+          //最小のS[i] を見て、同じ要素があればm[j]とcntを合わせて覚える
+          cnt++;
+          ans[m[j]] = cnt;
+        }
+      }
+    }
+    S.erase(*begin(S));
+  }
+
   
-  //↓の処理に有効
-  //集合の中にある値が含まれるかを高速に計算する
-  //集合の中に含まれる最大値、最小値を求める
-  //値の種類数
 
-  set<int> S;
 
-  S.insert(3);
-  S.insert(7);
-  S.insert(8);
-  S.insert(10);
+  
 
-  S.insert(3);
-
-  cout << "size: " << S.size() << endl;
-  //7が含まれるか => Set優位な処理
-  if (S.count(7))  {
-    cout << "found 7" << endl;
-  }
-  if (S.count(5)) {
-    cout << "found 5" << endl;
-  }
 }
