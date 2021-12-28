21de1dd2df2200030c865718f3a650885813ffe0
goro-o56 <yamashitagoro2727@gmail.com>
2021-12-28 17:41:47 +0900

    最頻値 X

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -2,40 +2,31 @@
 using namespace std;
 
 int main() {
-  //出現回数が最も多い値とその出現回数
-  //配列をしまう
-  map<int, int> m;
-  int N = 0;
-  set<int> S ;
-
-  cin >> N ;
-  for(int i = 0; i < N; i++) {
-    //mにkey(=i),valueをしまう
-    cin >> m[i];
-    S.insert(m[i]);
+  int N;
+  cin >> N;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A.at(i);
   }
 
-  //辞書の中で最も多く出現する物を総なめする
-  map<int, int> ans; //カウント用のmap
-  int cnt ;
-
-  for(int i = 0; i < S.size() ; i++) {//種類をそうなめ
-    cnt = 0;
-    for(int j = 0; j < N; j++) {
-      //ある最小の種類S[i]を固定した中で、m[j]が含まれるか
-        if(*begin(S) == m[j]){ //最小値から取っていけば順に進められる
-          //最小のS[i] を見て、同じ要素があればm[j]とcntを合わせて覚える
-          cnt++;
-          ans[m[j]] = cnt;
-        }
-      }
+  map<int, int> cnt;
+  for (int x : A) {
+    if (cnt.count(x)) {
+      cnt.at(x)++; 
+    }else {
+      cnt[x] = 1;
     }
-    S.erase(*begin(S));
+  }
+  
+  int max_cnt = 0;
+  int ans = -1;
+  for (int x : A) {
+    if(max_cnt < cnt.at(x)){
+      max_cnt = cnt.at(x);
+      ans = x;
+    }
   }
 
-  
-
-
-  
+  cout << ans << "" << max_cnt << endl;
 
 }
