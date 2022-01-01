177ca9453b3870aa7b5235f0eadd85c8bb2ee1ce
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-01 10:41:44 +0900

    constructor

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1,32 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct MyPair  {
+  int x;
+  string y;
+
+  MyPair() {
+    cout << "初期化無し" << endl;
+  }
+
+  MyPair(int x_) {
+    cout << "xのみ初期化" << endl;
+    x = x_;
+  }
+
+  MyPair(int x_,string y_) {
+    cout << "x,yともに初期化" << endl;
+    x = x_;
+    y = y_;
+  }
+};
+
 int main() {
-  int N;
-  cin >> N;
-  vector<int> A(N);
-  for (int i = 0; i < N; i++) {
-    cin >> A.at(i);
-  }
+  MyPair p;
+  cout << "p.x = " << p.x << endl;
+  cout << "p.y = " << p.y << endl;
 
-  map<int, int> cnt;
-  for (int x : A) {
-    if (cnt.count(x)) {
-      cnt.at(x)++; 
-    }else {
-      cnt[x] = 1;
-    }
-  }
-  
-  int max_cnt = 0;
-  int ans = -1;
-  for (int x : A) {
-    if(max_cnt < cnt.at(x)){
-      max_cnt = cnt.at(x);
-      ans = x;
-    }
-  }
+  MyPair q(6789);
+  cout << "q.x = " << q.x << endl;
+  cout << "q.y = " << q.y << endl;
 
-  cout << ans << "" << max_cnt << endl;
-
-}
+  MyPair r(11111, "good bye");
+  cout << "r.x = " << r.x << endl;
+  cout << "r.y = " << r.y << endl;
+ }
