be9df3c1f4c4aa67c68548af2a05abffdaba590d
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-02 14:24:48 +0900

    bitの操作

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -2,21 +2,13 @@
 using namespace std;
  
 int main() {
-  bitset<8> a("00011011");
-  bitset<8> b("00110101");
+  bitset<4> S;
+  S.set(0,1);
+  cout << S << endl;
 
-  auto c = a&b; //aとbのビット演算 and
-
-  cout << "1: " << c << endl;
-  cout << "2: " << (c << 1) << endl;
-  cout << "3: "<< (c << 2) << endl;
-  cout << "4: "<< (c << 3) << endl;
-  cout << "5: " << (c << 4) << endl;
-
-  c <<= 4;
-  cout << c << endl;
-
-  c ^= bitset<8>("11010000"); //cと11010000のxor
-  cout << "6: " << c << endl;
-
+  if(S.test(3)) {
+    cout << "4th bit is 1" << endl;
+  }else{
+    cout << "4th bit is 0" << endl;
+  }
 }
