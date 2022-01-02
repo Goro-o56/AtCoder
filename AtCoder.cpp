2996edc944457002403af8cc7d11fa3298c4d358
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-02 20:48:31 +0900

    再起関数とラムダ式

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1,35 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-int main() { 
-  int N, K;
-  cin >> N >> K;
-  vector<int> A(N);
-  for (int i = 0; i < N; i++){
-    cin >> A.at(i);
-  }
-  bool ans = false;
-  
-  //全ての選び方を試して、総和がKになるものがあるかを調べる
-  for (int tmp = 0; tmp < (1 << 20); tmp++) {
-    bitset<20> s(tmp); //20個の数についてを20ビットであらわす
 
-    //ビット列の1ビットに対応する整数をAから選びsumに足す
-    int sum = 0;
-    for (int i = 0; i < N; i++) {
-      if(s.test(i)) {
-        sum += A.at(i);
-      }
-    }
-    if (sum == K) {
-      ans = true;
-    }
-  }
+int main() {
+  function<int(int) > sum = [&](int n) {
+    if(n == 0){
+      return 0;
+    } 
+    int s = sum (n - 1);
+    return s + n;
+  };
 
-  if (ans) {
-    cout << "YES" << endl;
-  }else {
-    cout << "NO" <<endl;
-  }
-
+  cout << sum(3) << endl;
 }
