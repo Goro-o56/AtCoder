7c85e8ac0dd255c1219ef3d00ad32455a0a427f9
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-02 14:48:29 +0900

    bit 全探索 テスト

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -2,9 +2,34 @@
 using namespace std;
  
 int main() { 
-  //2のk乗を得るために1<<kと書くことがある
-  for (int tmp = 0; tmp < (1 << 3); tmp++) {
-    bitset<3> s(tmp);
-    cout << s << endl;
+  int N, K;
+  cin >> N >> K;
+  vector<int> A(N);
+  for (int i = 0; i < N; i++){
+    cin >> A.at(i);
   }
+  bool ans = false;
+  
+  //全ての選び方を試して、総和がKになるものがあるかを調べる
+  for (int tmp = 0; tmp < (1 << 20); tmp++) {
+    bitset<20> s(tmp); //20個の数についてを20ビットであらわす
+
+    //ビット列の1ビットに対応する整数をAから選びsumに足す
+    int sum = 0;
+    for (int i = 0; i < N; i++) {
+      if(s.test(i)) {
+        sum += A.at(i);
+      }
+    }
+    if (sum == K) {
+      ans = true;
+    }
+  }
+
+  if (ans) {
+    cout << "YES" << endl;
+  }else {
+    cout << "NO" <<endl;
+  }
+
 }
