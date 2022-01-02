a2f2bf9663114ec5c093f5b4135359035cc2852a
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-02 14:37:48 +0900

    ビット全探索

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -1,14 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
  
-int main() {
-  bitset<4> S;
-  S.set(0,1);
-  cout << S << endl;
-
-  if(S.test(3)) {
-    cout << "4th bit is 1" << endl;
-  }else{
-    cout << "4th bit is 0" << endl;
+int main() { 
+  //2のk乗を得るために1<<kと書くことがある
+  for (int tmp = 0; tmp < (1 << 3); tmp++) {
+    bitset<3> s(tmp);
+    cout << s << endl;
   }
 }
