d44591b84d106bdcd5a8b79781a4de5a4e80ab06
goro-o56 <yamashitagoro2727@gmail.com>
2022-01-03 03:37:08 +0900

    goto文 宣言について

--- AtCoder.cpp
+++ AtCoder.cpp
@@ -2,13 +2,9 @@
 using namespace std;
 
 int main() {
-  vector<int> v = {2, 1, 3};
-  sort(v.begin(), v.end());
-  do {
-    for(int x : v){
-      cout << x << " ";
-    }
-    cout << endl;
-  }
-  while(next_permutation(v.begin(), v.end()));
+  cout << "Hello, ";
+  goto SKIP;
+  cout << "world" << endl; //飛ばされる ラベル先まで
+SKIP:
+  cout << "AtCoder" << endl;
 }
